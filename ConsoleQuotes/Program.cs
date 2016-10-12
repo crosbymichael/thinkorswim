4cff308d81e8fcd17c2e55eb0c2b580b9062196e
Michael Crosby <crosbymichael@gmail.com>
2016-10-11 21:56:57 -0700

    Add console application
    Add a console applications for testing the package.

---
+++ ConsoleQuotes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkOrSwim;
+
+namespace ConsoleQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client();
+            client.Add("SPX", QuoteType.Last);
+
+            foreach(var quote in client.Quotes())
+            {
+                Console.WriteLine("{0} {1}: ${2}", quote.Symbol, quote.Type, quote.Value);
+            }
+        }
+    }
+}
